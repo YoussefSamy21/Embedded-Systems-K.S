d46068114f8c2d985fd11a81c0b49c227ae71a5e
Youssef Samy <youssef.samy@gmail.com>
2023-07-24 05:37:51 +0300

    edited functions assignment

--- Assignments/C Programming/Assignment 3/EX1.c
+++ Assignments/C Programming/Assignment 3/EX1.c
@@ -6,8 +6,9 @@ void findPrime(int n1,int n2);
 void main()
 {
 	int n1,n2;
-	printf("Enter two numbers (Intervals): ");
-	scanf("%d %d",&n1,&n2);
+	printf("Enter two numbers (Intervals):\n");
+	scanf("%d",&n1);
+	scanf("%d",&n2);
 	findPrime(n1,n2);
 }
 
@@ -15,9 +16,13 @@ void findPrime(int n1,int n2)
 {
 	int flag = 0,count = 2;
 	printf("Prime numbers between %d and %d are: ",n1,n2);
+	if(n1 == 1 || n1 == 0)
+	{
+		n1 = 2;
+	}
 	while(n1 <= n2)
 	{
-		while(count != n1)
+		while(count < n1)
 		{
 			if( (n1 % count) == 0)
 			{
