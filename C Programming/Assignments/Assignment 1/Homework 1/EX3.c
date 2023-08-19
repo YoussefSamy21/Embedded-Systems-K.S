8ae84e74c2bb3155d9e0539710b7c54d4811c175
Youssef Samy <youssef.samy@gmail.com>
2023-08-19 20:15:09 +0300

    C_Basics Assignment Part-1

---
+++ C Programming/Assignments/Assignment 1/Homework 1/EX3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+// C Program to Add Two Integers
+int main()
+{
+	int num1,num2;
+	printf("Enter 2 Integers: ");
+	scanf("%d %d",&num1,&num2);
+	printf("Sum: %d",num1+num2);
+	
+	return 0;
+}
---
+++ C Programming/Assignments/Assignment 1/Homework 1/EX4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+// C Program to Multiply two Floating Point Numbers
+int main()
+{
+	float num1,num2;
+	printf("Enter 2 Integers: ");
+	scanf("%f %f",&num1,&num2);
+	printf("Product: %f",num1*num2);
+	
+	return 0;
+}
