61e8982bd8892697cafee7de7442c0c7296b4b6b
Youssef Samy <youssef.samy@gmail.com>
2024-02-01 05:21:56 +0200

    Debug test

---
+++ Unit_8_Comm_Protocols/Lec5_I2C/Debug/I2C_interface.d
@@ -0,0 +1,13 @@
+I2C_interface.o I2C_interface.o: ../I2C_interface.c ../I2C_interface.h \
+  ../Utils/STD_LIBRARY.h ../Utils/STD_TYPES.h ../I2C_MemMap.h \
+  ../I2C_Config.h
+
+../I2C_interface.h:
+
+../Utils/STD_LIBRARY.h:
+
+../Utils/STD_TYPES.h:
+
+../I2C_MemMap.h:
+
+../I2C_Config.h:
